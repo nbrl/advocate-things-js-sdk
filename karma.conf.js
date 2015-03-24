d37cede246955b87ff65de21f9f6d31be84bd387
Nathan Burrill <nathan@digitalanimal.com>
2015-03-24 15:57:11 +0000

    use concatenated sdk.js

--- karma.conf.js
+++ karma.conf.js
@@ -22,7 +22,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      './lib/*.js',
+      './dest/*.js',
       './test/*.js'
     ],
 
