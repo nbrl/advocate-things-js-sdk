9b32a70d754e3dfae68bd0bf708ee0e7e8f12747
Nathan <nathan@digitalanimal.com>
2015-05-27 12:19:20 +0100

    some more tests and more working

--- src/sdk.js
+++ src/sdk.js
@@ -1,7 +1,40 @@
 ;(function(context) {
 
     var AT = {};
+
     var scriptId = 'advocate-things-script';
+    var storageName = 'advocate-things';
+    var points = {
+        Sharepoint: {
+            name: 'Sharepoint',
+            url: 'https://sharepoint-data-collector.herokuapp.com/sharepoint/data'
+        },
+        Touchpoint: {
+            name: 'Touchpoint',
+            url: 'https://touchpoint-data-collector.herokuapp.com/touchpoint/data'
+        },
+        Unknown: {
+            name: 'Unknown'
+        }
+    };
+
+    AT.Events = {
+        SharepointSaved: 'SharepointSaved',
+        TouchpointSaved: 'TouchpointSaved',
+        ReferredPerson: 'ReferredPerson'
+    };
+
+
+    /**
+     * Injection of polyfils and third-party libraries
+     */
+
+    var utils = {};
+    (function () {
+        /* inject */
+        /* endinject */
+    }).call(utils);
+
 
     /**
      * Internal function definitions
@@ -22,11 +55,48 @@
         return null;
     };
 
+    AT._prepareData = function (data) {
+        var requiredProps = [ '_at', '_client' ];
+        var tidy; // Will contain tidied data.
+
+        // Ensure data is a real object. Initialise if not.
+        if (!data || Object.prototype.toString.call(data) !== '[object Object]') {
+            tidy = {};
+        } else {
+            tidy = JSON.parse(JSON.stringify(data || {})); // clone data
+        }
+
+        for (propId in requiredProps) {
+            if (!tidy[requiredProps[propId]] || Object.prototype.toString.call(tidy[requiredProps[propId]]) !== '[object Object]') {
+                tidy[requiredProps[propId]] = {};
+            }
+        }
+
+        tidy._at.apiKey = AT._getApiKey();
+        tidy._at.fingerprint = 'foo';
+        tidy._at.url = window.location.href;
+
+        for (key in tidy) {
+            if (!(key === '_at' || key === '_client')) { // TODO: change to use requiredProps?
+                tidy._client[key] = tidy[key];
+                delete tidy[key];
+            }
+        }
+
+        return tidy;
+    };
+
 
     /**
      * Public function definitions
      */
 
+    AT.sendSharepoint = function (name, data, cb) {
+        if (!AT._getApiKey()) {
+            return null;
+        }
+
+    };
 
 
     context.AT = AT;
--- test/sdk.js
+++ test/sdk.js
@@ -1,9 +1,12 @@
 var expect = require('expect.js');
 var sinon = require('sinon');
+var _ = require('lodash');
 
 var scriptId = 'advocate-things-script';
 var scriptUrl = 'https://cloudfront.whatever/bucket/sdk.js';
 var apiKey = 'foobar';
+
+var _getApiKeyStub;
 
 describe('the SDK', function () {
 
@@ -72,13 +75,203 @@ describe('the SDK', function () {
 
 
 
+    describe('_prepareData()', function () {
+
+        beforeEach(function () {
+            _getApiKeyStub = sinon.sandbox.stub(window.AT, '_getApiKey');
+            _getApiKeyStub.returns(apiKey);
+        });
+
+        function checkMinObj (data, hasMeta) {
+            expect(data._at).to.be.an('object');
+            expect(data._at.apiKey).to.equal(apiKey);
+            expect(data._at.fingerprint).to.be.a('string');
+            expect(data._at.url).to.match(/^http[s]*:\/\//);
+            if (hasMeta) {
+                expect(data._client).to.be.an('object');
+            } else {
+                expect(data._client).to.eql({});
+            }
+        }
+
+        it('should return a minimally initialised object if called with non-[object Object]', function () {
+            var args = [ null, undefined, 'string', [ 'array' ]];
+
+            for (var i=0; i<args.length; i++) {
+                checkMinObj(AT._prepareData(args[i]));
+            }
+        });
+
+        it('should return a minimally initialised object if called with an empty object', function () {
+	    checkMinObj(AT._prepareData({}));
+        });
+
+        it('should return a minimally initialised object if the provided data has no _at but has meta', function () {
+	    var data = {
+                foo: {
+                    bar: 'baz'
+                }
+            };
+
+            var res = AT._prepareData(data);
+            checkMinObj(res, true);
+            expect(res._client).to.eql(data);
+        });
+
+        it('should not clobber extra valid parameters in the _at object', function () {
+	    var emailAddress = 'johnsmith@example.com';
+            var data = {
+                _at: {
+                    email: emailAddress
+                }
+            };
+
+            var res = AT._prepareData(data);
+            checkMinObj(res, true);
+            expect(res._at.email).to.equal(emailAddress);
+        });
+
+        it('should not clobber extra invalid parameters in the _at object', function () {
+            var randomValue = 'some nonsense goes in here';
+            var paramName = 'randomUnsupported_parameter';
+            var data = {
+                _at: {}
+            };
+            data._at[paramName] = randomValue;
+
+            var res = AT._prepareData(data)
+
+            // Check it still adheres to the minimal spec
+            checkMinObj(res);
+
+            // Check supplied params not clobbered
+            expect(res._at[paramName]).to.equal(randomValue);
+        });
+
+        it('should correctly tidy the object when client metadata is present', function () {
+            var emailAddress = 'johnsmith@example.com';
+            var meta = {
+                transaction: {
+                    amount: 50.50,
+                    products: [
+                        'product-1',
+                        'product-2',
+                        'firetruck'
+                    ]
+                }
+            };
+            var data = {
+                _at: {
+                    email: emailAddress
+                }
+            };
+            _.extend(data, meta);
+
+            var res = AT._prepareData(data);
+
+            // Check it still adheres to the minimal spec (including metadata)
+            checkMinObj(res, true);
+
+            expect(res._client).to.eql(meta);
+        });
+
+        it('should not touch the object when _client object exists and no new data is present', function () {
+            var data = {
+                _at: {
+                    email: 'johnsmith@example.com'
+                },
+                _client: {
+                    transaction: {
+                        amount: 24.99,
+                        items: [ 'one', 'two' ],
+                        order_id: '12352341'
+                    }
+                }
+            };
+
+            var res = AT._prepareData(data);
+
+            // Check it still adheres to the minimal spec (including metadata)
+            checkMinObj(res, true);
+
+            expect(res._client).to.eql(data._client);
+        });
+
+        it('should correctly insert any new data when client metadata and a _client object exists', function () {
+            var data = {
+                _at: {
+                    email: 'johnsmith@example.com'
+                },
+                _client: {
+                    transaction: {
+                        amount: 24.99,
+                        items: [ 'one', 'two' ],
+                        order_id: '12352341'
+                    }
+                }
+            };
+            var extraData = {
+                user_facebook_id: 'fb1234'
+            };
+            _.extend(data, extraData);
+
+            var expectedClientObject = _.extend(data._client, extraData);
+
+            var res = AT._prepareData(data);
+
+            // Check it still adheres to the minimal spec (including metadata)
+            checkMinObj(res, true);
+
+            expect(res._client).to.eql(expectedClientObject);
+        });
+
+        it('should overwrite keys in the _client object if conflicting client metadata and _client object exists', function () {
+            var data = {
+                _at: {
+                    email: 'johnsmith@example.com'
+                },
+                _client: {
+                    transaction: {
+                        amount: 24.99,
+                        items: [ 'one', 'two' ],
+                        order_id: '12352341'
+                    }
+                }
+            };
+            var extraData = {
+                transaction: {
+                    amount: 15.49
+                }
+            };
+            _.extend(data, extraData);
+
+            // Note that this replaces the whole transaction object.
+            var expectedClientObject = _.extend(data._client, extraData);
+
+            var res = AT._prepareData(data);
+
+            // Check it still adheres to the minimal spec (including metadata)
+            checkMinObj(res, true);
+
+            expect(res._client).to.eql(expectedClientObject);
+        });
+
+    });
+
+
+
     describe('sendSharepoint()', function () {
 
-        it('should return null if there is no api key', function () {
-	    var getApiKeyStub = sinon.sandbox.stub(window.AT, '_getApiKey');
-            getApiKeyStub.returns(apiKey);
+        beforeEach(function () {
+            _getApiKeyStub = sinon.sandbox.stub(window.AT, '_getApiKey');
+            _getApiKeyStub.returns(apiKey);
+        });
 
-            expect(AT._getApiKey()).to.equal(apiKey);
+        xit('should return null if there is no api key', function () {
+            _getApiKeyStub.returns(null);
+
+            //expect(AT.sendSharepoint()).to.be(null);
+            expect(true).to.be(true);
         });
 
     });
